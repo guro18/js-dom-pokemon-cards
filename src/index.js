791bc49862ff2c97ad15bf430bab3297cf75685c
guro18 <rollgustav95@gmail.com>
2024-11-19 10:43:00 +0100

    extensions completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-console.log("data", data);
-
-//You can start simple and just render a single 
-//pokemon card from the first element
-
 //cardcontainer
 const cardContainer = document.createElement("ul");
 cardContainer.classList.add("card-container");
@@ -13,10 +8,10 @@ for (let i = 0; i < data.length; i++){
     const cardListItem = document.createElement("li");
     cardListItem.classList.add("card");
 
-// Display the name of the Pokemon
+//display the name of the Pokemon
 const pokemonName = document.createElement("h2");
 pokemonName.classList.add("card--title");
-pokemonName.textContent = data[i].name;
+pokemonName.textContent = data[i].name.toLocaleUpperCase();
 cardListItem.appendChild(pokemonName);
 
 //display the image
@@ -42,7 +37,7 @@ cardContainer.appendChild(cardListItem);
 
 //games
 const gametitle = document.createElement("h3");
-gametitle.textContent = ('Games');
+gametitle.textContent = ('Games:');
 const gameList = document.createElement("ul");
 gameList.classList.add("card--text");
 
@@ -53,6 +48,17 @@ data[i].game_indices.forEach(game => {
 });
 cardListItem.appendChild(gametitle);
 cardListItem.appendChild(gameList);
+
+//function for toggleswitch
+function changeImage() {
+    if (pokemonImage.src === data[i].sprites.other["official-artwork"].front_default){
+        pokemonImage.src = data[i].sprites.other.dream_world.front_default;
+    } else {
+        pokemonImage.src = data[i].sprites.other["official-artwork"].front_default;
+    }
+}
+
+cardListItem.addEventListener("click", changeImage);
 
 //additional styling for the layout
 const cardWidth = (100 / 3) + '%';
